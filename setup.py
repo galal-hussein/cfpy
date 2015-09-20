43d91f1005d836713a7154a0c1ed0a49076b6163
Hussein Galal <hussein.galal.ahmed.11@gmail.com>
2015-09-20 04:06:45 +0200

    add version 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='cfpy',
-    version='0.1',
+    version='0.2',
     description='CloudFlare V4 Python Wrapper',
     author='Hussein Galal',
     author_email='hussein.galal.ahmed.11@gmail.com',
